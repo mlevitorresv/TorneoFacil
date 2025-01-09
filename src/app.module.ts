84e74638f45c5550906c07426a1f4fda6a6a64fd
mlevitorresv <levitorres66@gmail.com>
2025-01-09 11:50:57 +0100

    prisma module añadido a app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { TeamsModule } from './teams/teams.module';
 import { PlayersModule } from './players/players.module';
 import { CoachesModule } from './coaches/coaches.module';
 import { MatchesModule } from './matches/matches.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [TeamsModule, PlayersModule, CoachesModule, MatchesModule],
+  imports: [PrismaModule, TeamsModule, PlayersModule, CoachesModule, MatchesModule],
 })
 
 export class AppModule { }
