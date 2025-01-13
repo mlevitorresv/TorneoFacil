4f65432391e95b8401d7f3d7e9e60ca5701511d7
mlevitorresv <levitorres66@gmail.com>
2025-01-13 15:02:58 +0100

    servicio (coaches) con acceso a BBDD y correcta gestión de los datos

--- src/coaches/controllers/coaches.controller.ts
+++ src/coaches/controllers/coaches.controller.ts
@@ -1,31 +1,34 @@
-import { Controller, Delete, Get, HttpCode, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put } from '@nestjs/common';
 import { CoachesService } from '../services/coaches.service';
+import { CreateCoachDto } from '../dto/create-coach.dto';
+import { UpdateCoachDto } from '../dto/update-coach.dto';
 
 @Controller('coaches')
 export class CoachesController {
     constructor(private readonly coachesService: CoachesService) { }
 
     @Get()
-    getAll() {
-        return this.coachesService.getAll();
-    }
-    @Get(':id')
-    getOne( @Param() params ) {
-        return this.coachesService.getOne(params.id);
-    }
-
-    @Post()
-    create() {
-        return this.coachesService.create();
-    }
-
-    @Delete(':id')
-    delete( @Param() params ) {
-        return this.coachesService.delete(params.id);
-    }
-
-    @Put(':id')
-    update( @Param() params ){
-        return this.coachesService.update(params.id);
-    }
+    @Get()
+        getAll() {
+            return this.coachesService.getAll()
+        }
+        @Get(':id')
+        getOne( @Param() params ) {
+            return this.coachesService.getOne(parseInt(params.id))
+        }
+    
+        @Post()
+        create(@Body() data: CreateCoachDto) {
+            return this.coachesService.create(data)
+        }
+    
+        @Delete(':id')
+        delete( @Param() params ) {
+            return this.coachesService.delete(parseInt(params.id))
+        }
+    
+        @Put(':id')
+        update( @Param() params, @Body() data: UpdateCoachDto ){
+            return this.coachesService.update(parseInt(params.id), data)
+        }
 }
--- src/coaches/services/coaches.service.ts
+++ src/coaches/services/coaches.service.ts
@@ -1,20 +1,48 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateCoachDto } from '../dto/create-coach.dto';
+import { UpdateCoachDto } from '../dto/update-coach.dto';
 
 @Injectable()
 export class CoachesService {
-  getAll(): string {
-    return 'obtención de todos los entrenadores';
-  }
-  getOne(id: number): string {
-    return 'obtención de un entrenador por el id: ' + id
-  }
-  delete(id: number): string {
-    return 'eliminación de entrenador por id: ' + id
-  }
-  create(): string {
-    return 'creación de entrenador'
-  }
-  update(id: number): string {
-    return 'actualización de entrenador: ' + id
-  }
+  constructor(private readonly prisma: PrismaService){}
+  
+  
+    async getAll() {
+      const result = await this.prisma.coach.findMany()
+      return result;
+    }
+  
+    async getOne(id: number) {
+      const result = await this.prisma.coach.findUnique({
+        where: {
+          id: id
+        }
+      })
+      return result
+    }
+  
+    async delete(id: number) {
+      const result = await this.prisma.coach.delete({
+        where: {
+          id: id
+        }
+      })
+      return result
+    }
+  
+    async create(data: CreateCoachDto) {
+      const result = await this.prisma.coach.create({data})
+      return result
+    }
+  
+    async update(id: number, data: UpdateCoachDto) {
+      const result = await this.prisma.coach.update({
+        data: data,
+        where: {
+          id: id
+        }
+      })
+      return result
+    }
 }
